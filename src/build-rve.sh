77f393b4d7e0ef9fadaf549fc4175b383e51cc4b
3r1pm4V13 <134662783+3r1pm4V13@users.noreply.github.com>
2023-09-07 08:02:48 +0700

    Update build-rve.sh

--- src/build-rve.sh
+++ src/build-rve.sh
@@ -17,8 +17,8 @@ patch "youtube" "youtube-revanced-extended-v$version"
 
 # Patch YouTube Music Extended 
 get_patches_key "youtube-music-revanced-extended"
-#get_apkmirror "youtube-music" "arm64-v8a"
-get_uptodown "youtube-music" 
+get_apkmirror "youtube-music" "arm64-v8a"
+#get_uptodown "youtube-music" 
 patch "youtube-music" "youtube-music-revanced-extended-v$version"
 
 #Reddit
